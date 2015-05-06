829957fe807dec3231d479bc5bbf575b7a7cb792
Peter Liniker <pliniker@jessie.buildfaxdb.com>
2015-05-06 15:41:24 -0400

    switch from xmobar to taffybar which has a builtin systray

--- xmonad.hs
+++ xmonad.hs
@@ -1,21 +1,25 @@
+import System.IO
 
 import XMonad
-import XMonad.Actions.CycleWindows -- classic alt-tab
-import XMonad.Actions.CycleWS      -- cycle thru WS', toggle last WS
-import XMonad.Hooks.DynamicLog     -- statusbar
-import XMonad.Hooks.EwmhDesktops   -- fullscreenEventHook fixes chrome fullscreen
-import XMonad.Hooks.ManageDocks    -- dock/tray mgmt
-import XMonad.Hooks.UrgencyHook    -- window alert bells
-import XMonad.Layout.Named         -- custom layout names
-import XMonad.Layout.NoBorders     -- smart borders on solo clients
-import XMonad.Util.EZConfig        -- append key/mouse bindings
-import XMonad.Util.Run(spawnPipe)  -- spawnPipe and hPutStrLn
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.UrgencyHook
+import XMonad.Layout.Named
+import XMonad.Layout.NoBorders
+import XMonad.Util.EZConfig
+import XMonad.Util.Run(spawnPipe)
 import qualified XMonad.StackSet as W
-import XMonad.Layout.IndependentScreens -- dwm style workspaces
-import System.IO                   -- hPutStrLn scope
+import XMonad.Layout.IndependentScreens
+
+import System.Taffybar.XMonadLog (dbusLogWithPP, taffybarDefaultPP,
+                                   taffybarColor, taffybarEscape)
+
+import DBus
+import DBus.Client
 
 
-conf status monitors = defaultConfig
+conf monitors dbus = defaultConfig
             { modMask = mod4Mask
             , terminal = "xfce4-terminal"
             , borderWidth = 3
@@ -25,21 +29,13 @@ conf status monitors = defaultConfig
             , workspaces = withScreens monitors wsNames
             , layoutHook = myLayoutHook
             , manageHook = manageDocks <+> myManageHook
-                            <+> manageHook defaultConfig
-            , logHook = myLogHook status
+                           <+> manageHook defaultConfig
+            , logHook = dbusLogWithPP dbus myXmobarPP
             }
             `additionalKeysP` myKeys
-            `additionalMouseBindings` myButtons
             `additionalKeys` wsKeys
 
-main = do
-        status <- spawnPipe "xmobar ~/.xmonad/xmobarrc"
-        tools <- spawnPipe "~/.xmonad/xsession"
-        monitors <- countScreens
-        xmonad $ withUrgencyHook NoUrgencyHook $ conf status monitors
-
 -- Tags/Workspaces
---
 wsNames = map show [1 .. 9 :: Int]
 
 -- Layouts
@@ -52,40 +48,30 @@ myLayoutHook = avoidStruts $ smartBorders ( tiled ||| mtiled ||| full )
     -- sets default tile as: Tall nmaster (delta) (golden ratio)
 
 -- Window management
---
 myManageHook = composeAll
     [ className =? "MPlayer" --> doFloat
     , className =? "Vlc" --> doFloat
     , className =? "Gimp" --> doFloat
     , className =? "XCalc" --> doFloat
-    , className =? "stalonetray" --> doIgnore
     ]
 
 -- Statusbar
---
-myLogHook h = dynamicLogWithPP $ myXmobarPP { ppOutput = hPutStrLn h }
-
-myXmobarPP = xmobarPP
-    { ppCurrent = xmobarColor "#3399ff" "" . wrap " " " "
---    , ppHidden = xmobarColor "#dddddd" "" . wrap " " " "
---    , ppHiddenNoWindows = xmobarColor "#777777" "" . wrap " " " "
-    , ppUrgent = xmobarColor "#ff0000" "" . wrap " " " "
-    , ppSep = " "
-    , ppWsSep = "/"
-    , ppLayout = xmobarColor "#aaaaaa" "" . wrap "·" "·"
-    , ppTitle = xmobarColor "#ffffff" "" . shorten 25
-    }
+blank _ = ""
+myXmobarPP = taffybarDefaultPP
+   {
+     ppCurrent = taffybarColor "#f8f8f8" "DodgerBlue4" . wrap " " " "
+   , ppVisible = taffybarColor "#f8f8f8" "LightSkyBlue4" . wrap " " " "
+   , ppUrgent  = taffybarColor "#f8f8f8" "red4" . wrap " " " "
+   , ppHidden = blank
+   , ppHiddenNoWindows = blank
+   , ppWsSep = " : "
+   , ppLayout  = taffybarColor "DarkOrange" "" . wrap " [" "] "
+   , ppTitle   = taffybarColor "#ffffff" "" . shorten 150
+   }
 
 -- Key bindings
---
-myKeys = [ ("M-b"        , sendMessage ToggleStruts              ) -- toggle the status bar gap
-         , ("M1-<Tab>"   , cycleRecentWindows [xK_Alt_L] xK_Tab xK_Tab ) -- classic alt-tab behaviour
-         , ("M-<Tab>"    , toggleWS                              ) -- toggle last workspace (super-tab)
-         , ("M-<Right>"  , nextWS                                ) -- go to next workspace
-         , ("M-<Left>"   , prevWS                                ) -- go to prev workspace
-         , ("M-S-<Right>", shiftToNext                           ) -- move client to next workspace
-         , ("M-S-<Left>" , shiftToPrev                           ) -- move client to prev workspace
-         , ("C-M1-l"     , spawn "xscreensaver-command --lock"   ) -- lock screen
+myKeys = [ ("M-b", sendMessage ToggleStruts)
+         , ("C-M1-l", spawn "xscreensaver-command --lock")
          ]
 
 -- Workspace keys
@@ -95,10 +81,8 @@ wsKeys = [
               , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
          ]
 
--- Mouse bindings
---
-myButtons = [ ((0, 8), (\_ -> prevWS )) -- cycle workspaces
-            , ((0, 9), (\_ -> nextWS )) -- with thumb buttons
-            ]
-
--- vim:sw=4 sts=4 ts=4 tw=0 et ai
+main = do
+  dbus <- connectSession
+  tools <- spawnPipe "~/.xmonad/xsession"
+  monitors <- countScreens
+  xmonad $ withUrgencyHook NoUrgencyHook $ conf monitors dbus
