1207ea868808dd2d1ef6844d905ab4603eb12951
Peter Liniker <pliniker@buildfax.com>
2017-09-15 10:06:51 -0400

    Favorite packages in install script

--- install.sh
+++ install.sh
@@ -1,9 +1,23 @@
-apt install xmonad taffybar dmenu notify-osd gnome-terminal nitrogen feh xfce4-volumed
+#!/bin/bash -ex
+
+sudo apt install -y git openssh-server ibam cpufrequtils xmonad taffybar dmenu notify-osd gnome-terminal gnome-tweak-tool gnote nitrogen feh fdpowermon xfce4-volumed htop emacs vim-nox python-gpgme build-essential curl tzwatch
 
 #
-# To dump and restore gnome-terminal profiles from another machine:
+# Turn off dnsmasq:
+#
+# sudo vim /etc/NetworkManager/NetworkManager.conf
+# comment-out dns=dnsmasq
+# sudo service network-manager restart
+#
+
+#
+# Install spacemacs
+#
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+
+#
+# To dump and restore gnome-terminal profiles:
 #
 # dconf dump /org/gnome/terminal/legacy/profiles:/ > gnome-term.conf
 #
-# cat gnome-term.conf | dconf load /org/gnome/terminal/legacy/profiles:/
-#
+cat gnome-term.conf | dconf load /org/gnome/terminal/legacy/profiles:/
