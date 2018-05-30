8f96a932b31d16877af186c3dd22ef6df4d42e8a
Peter Liniker <pliniker@buildfax.com>
2018-05-30 15:34:59 -0400

    Add powerline, turn off compton

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
-sudo apt install -y git openssh-server ibam cpufrequtils xmonad taffybar dmenu notify-osd gnome-terminal gnome-tweak-tool gnote nitrogen feh fdpowermon xfce4-volumed htop emacs vim-nox python-gpgme build-essential curl tzwatch
+sudo apt install -y git openssh-server fish xmonad taffybar dmenu notify-osd gnome-terminal gnome-tweak-tool gnote nitrogen feh fdpowermon xfce4-volumed htop emacs vim-nox python-gpgme build-essential curl ibam
 
 #
 # Turn off dnsmasq:
@@ -24,3 +24,12 @@ cat gnome-term.conf | dconf load /org/gnome/terminal/legacy/profiles:/
 
 mkdir -p ~/.config/gtk-3.0/
 cp gtk3-settings.ini ~/.config/gtk-3.0/settings.ini
+
+#
+# Install powerline, adjusting REPO_ROOT as appropriate
+#
+pip3 install --user powerline-status
+
+REPO_ROOT=~/.local/lib/python3.5/site-packages/ 
+echo set fish_function_path \$fish_function_path "$REPO_ROOT/powerline/bindings/fish" >> ~/.config/fish/config.fish
+echo powerline-setup >> ~/.config/fish/config.fish
