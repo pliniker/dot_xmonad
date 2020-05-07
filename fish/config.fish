b9504f52be59c340b72810625b13d097e2b01bf7
Peter Liniker <pliniker@buildfax.com>
2020-05-07 15:23:45 -0400

    Use spacevim and a vanilla alt vimrc

--- fish/config.fish
+++ fish/config.fish
@@ -5,12 +5,15 @@ set -x NO_AT_BRIDGE 1
 
 set -x TERM 'xterm-256color'
 
+set fish_function_path $fish_function_path "/home/pliniker/.local/lib/python3.6/site-packages/powerline/bindings/fish"
+powerline-setup
+
 alias ll="ls -ltrah"
 alias open="xdg-open"
 alias sbcl="rlwrap sbcl"
 alias hy="hy --repl-output-fn=hy.contrib.hy-repr.hy-repr"
 alias emacs="env TERM=xterm-24bit /usr/bin/emacs"
 
-set REPO_ROOT ~/.local/lib/python3.6/site-packages/
-set fish_function_path $fish_function_path "$REPO_ROOT/powerline/bindings/fish"
-powerline-setup
+
+# vanilla vim, don't load spacevim
+alias vim="vim.nox -u ~/.vimrc-vim.nox"
