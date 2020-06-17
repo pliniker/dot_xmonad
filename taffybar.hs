173faa3a160e8775dfb7c863ef46f30d30b0613a
Peter Liniker <peter.liniker+github@gmail.com>
2020-06-17 16:32:14 -0400

    update for 20.04

--- taffybar.hs
+++ taffybar.hs
@@ -1,60 +1,71 @@
--- the battery monitor will cause a fatal error if upower is not installed
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
 
-import System.Information.Memory
 import System.Taffybar
-import System.Taffybar.Battery
-import System.Taffybar.CPUMonitor
-import System.Taffybar.FreedesktopNotifications
-import System.Taffybar.FSMonitor
-import System.Taffybar.NetMonitor
-import System.Taffybar.Pager (colorize, escape)
-import System.Taffybar.SimpleClock
-import System.Taffybar.Systray
-import System.Taffybar.TaffyPager
-import System.Taffybar.Pager
-import System.Taffybar.Text.MemoryMonitor
-import System.Taffybar.Weather
-import System.Taffybar.Widgets.PollingGraph
-import System.Taffybar.WorkspaceHUD
+import System.Taffybar.Hooks
+import System.Taffybar.Information.CPU
+import System.Taffybar.Information.Memory
+import System.Taffybar.SimpleConfig
+import System.Taffybar.Widget
+import System.Taffybar.Widget.Generic.PollingGraph
+import System.Taffybar.Widget.Generic.PollingLabel
+import System.Taffybar.Widget.Util
+import System.Taffybar.Widget.Workspaces
 
+transparent = (0.0, 0.0, 0.0, 0.5)
+yellow1 = (0.9453125, 0.63671875, 0.2109375, 1.0)
+yellow2 = (0.9921875, 0.796875, 0.32421875, 1.0)
+green1 = (0, 1, 0, 1)
+green2 = (1, 0, 1, 0.5)
+taffyBlue = (0.129, 0.588, 0.953, 1)
+
+myGraphConfig =
+  defaultGraphConfig
+  { graphPadding = 0
+  , graphBorderWidth = 0
+  , graphWidth = 75
+  , graphBackgroundColor = transparent
+  }
+
+memCfg = myGraphConfig
+  { graphDataColors = [taffyBlue] }
+
+cpuCfg = myGraphConfig
+  { graphDataColors = [green1, green2] }
+
+memCallback :: IO [Double]
 memCallback = do
   mi <- parseMeminfo
-  return $ map ((/memoryTotal mi) . ($ mi)) [ memoryBuffer, memoryCache ]
+  return [memoryUsedRatio mi]
 
-blank _ = ""
-
-small s = "<span font_size='x-small'>" ++ s ++ "</span>"
-
-pagerConfig   = defaultPagerConfig
-                { activeWindow     = colorize "#f8f8f8" "black" . escape . shorten 40
-                , activeLayout     = colorize "DarkOrange" "black" . wrap "[" "]" . escape
-                , activeWorkspace  = colorize "black" "LightSkyBlue4" . wrap "[" "]" . escape
-                , hiddenWorkspace  = colorize "black" "SkyBlue4" . small . escape
-                , emptyWorkspace   = colorize "#777777" "black" . small . escape
-                , visibleWorkspace = wrap "(" ")" . escape
-                , urgentWorkspace  = colorize "#f8f8f8" "red4" . escape
-                , widgetSep        = " / "
-                }
+cpuCallback = do
+  (_, systemLoad, totalLoad) <- cpuLoad
+  return [totalLoad, systemLoad]
 
 main = do
-  let cpuCfg = defaultGraphConfig { graphDataColors = [ (0, 1, 0, 1), (1, 0, 1, 0.5)]
-                                  , graphLabel = Just "cpu"
-                                  }
-      memCfg = defaultGraphConfig { graphDataColors = [ (0, 1, 0, 1), (1, 0, 1, 0.5)]
-                                  , graphLabel = Just "mem"
-                                  }
-      clock = textClockNew Nothing "<span fgcolor='lightgray'>%a %b %_d %H:%M</span>" 1
-      pager = taffyPagerHUDNew pagerConfig (hudFromPagerConfig pagerConfig)
-      tray = systrayNew
-      cpu = cpuMonitorNew cpuCfg 2.0 "cpu"
-      mem = pollingGraphNew memCfg 4.0 memCallback
-      battery = batteryBarNew defaultBatteryConfig 60.0
-  defaultTaffybar defaultTaffybarConfig { startWidgets = [ pager ]
-                                        , endWidgets = [ tray
-                                                       , battery
-                                                       , clock
-                                                       , mem
-                                                       , cpu
-                                                       ]
-                                        , barHeight = 35
-                                        }
+  let myWorkspacesConfig =
+        defaultWorkspacesConfig
+        { maxIcons = Just 0 }
+      workspaces = workspacesNew myWorkspacesConfig
+      cpu = pollingGraphNew cpuCfg 2 cpuCallback
+      mem = pollingGraphNew memCfg 2 memCallback
+      clock = textClockNew Nothing "%a %b %_d %H:%M" 1
+      layout = layoutNew defaultLayoutConfig
+      windows = windowsNew defaultWindowsConfig
+      battText = textBatteryNew "$percentage$% $time$"
+      myConfig = defaultSimpleTaffyConfig
+        { startWidgets =
+            workspaces : map (>>= buildContentsBox) [ layout, windows ]
+        , endWidgets = map (>>= buildContentsBox)
+          [ battText
+          , clock
+          , cpu
+          , mem
+          ]
+        , barPosition = Top
+        , barPadding = 0
+        , barHeight = 25
+        , widgetSpacing = 5
+        }
+  dyreTaffybar $ withBatteryRefresh $ withLogServer $ withToggleServer $
+               toTaffyConfig myConfig
