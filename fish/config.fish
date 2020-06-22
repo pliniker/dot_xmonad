6c470bee1bb2ca8642485282b801837d30182506
Peter Liniker <peter.liniker+github@gmail.com>
2020-06-22 08:03:48 -0400

    Sync some changes from focal branch

--- fish/config.fish
+++ fish/config.fish
@@ -5,7 +5,6 @@ set -x PATH $HOME/.cargo/bin $PATH
 set -x NO_AT_BRIDGE 1
 
 set -x TERM 'xterm-256color'
-# set -x TERM 'xterm-24bit'
 
 set REPO_ROOT ~/.local/lib/python3.7/site-packages/
 set fish_function_path $fish_function_path "$REPO_ROOT/powerline/bindings/fish"
@@ -15,7 +14,7 @@ alias ll="ls -ltrah"
 alias open="xdg-open"
 alias sbcl="rlwrap sbcl"
 alias hy="hy --repl-output-fn=hy.contrib.hy-repr.hy-repr"
-alias emacs="env TERM=xterm-24bit /usr/bin/emacs"
+alias emacs="env TERM=xterm-24bits /usr/bin/emacs -nw"
 
 # vanilla vim, don't load spacevim
-alias vim="nvim -u ~/.vimrc-vim.nox"
+alias vim="vim.nox -u ~/.vimrc-vim.nox"
--- install.sh
+++ install.sh
@@ -1,22 +1,17 @@
 #!/bin/bash -ex
 
 #
-# Setup Debian repos
-#
-sudo cp -r debian/apt/ /etc/
-
-#
 # Install environment
 #
 sudo apt install -y \
     git openssh-server fish htop curl build-essential emacs-nox vim-nox \
-    xmonad taffybar dmenu gnome-flashback gnome-session-flashback gnome-terminal gnome-tweak-tool gnote nitrogen formiko \
+    xmonad taffybar dmenu gnome-flashback gnome-session-flashback gnome-terminal gnome-tweak-tool gnote nitrogen \
     ibam powertop tlp
 
 #
 # Setup gnome flashback session manager, prevent nautilus from auto-starting
 #
-sudo cp gnome-flashback-xmonad.session /usr/share/gnome-session/sessions/
+sudo cp gnome/gnome-flashback-xmonad.session /usr/share/gnome-session/sessions/
 sudo rm /etc/xdg/autostart/*nautilus*
 
 #
@@ -28,9 +23,10 @@ sudo rm /etc/xdg/autostart/*nautilus*
 cat gnome-term.conf | dconf load /org/gnome/terminal/legacy/profiles:/
 
 #
-# Install powerline, adjusting REPO_ROOT as appropriate
+# Install alacritty
 #
-pip3 install --user powerline-status
+# https://github.com/alacritty/alacritty/releases/latest
+cp -r alacritty ~/.config/
 
 #
 # Handy scripts
@@ -47,7 +43,7 @@ cp fish/config.fish ~/.config/fish/
 #
 # Install spacemacs
 #
-git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+git clone https://github.com/syl20bnr/spacemacs -b develop ~/.emacs.d
 git clone git@github.com/pliniker/dot_spacemacs ~/.spacemacs.d
 
 #
@@ -59,6 +55,7 @@ git clone git@github.com/pliniker/dot_spacevim_d ~/.SpaceVim.d
 #
 # Install python deps
 #
+pip3 install --user powerline-status
 pip3 install --user 'python-language-server[all]'
 
 #
@@ -80,4 +77,5 @@ tic -x -o ~/.terminfo terminfo-24bit.src
 #
 # Install tools
 #
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 cargo install battop
