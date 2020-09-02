d2f012566069c8172df03130eb625abded7be5cd
Peter Liniker <pliniker@buildfax.com>
2020-09-02 09:13:21 -0400

    Don't use powerline on wsl, too slow

--- fish/config.fish
+++ fish/config.fish
@@ -1,18 +1,17 @@
 set -x PATH $HOME/bin $PATH
 set -x PATH $HOME/.local/bin $PATH
-set -x PATH $HOME/.cargo/bin $PATH
 
 set -x NO_AT_BRIDGE 1
-
-set -x TERM 'xterm-256color'
-# set -x TERM 'xterm-24bit'
-
 set -x DISPLAY :0.0
 set -x LIBGL_ALWAYS_INDIRECT 1
+set -x TERM 'xterm-256color'
 
-set REPO_ROOT ~/.local/lib/python3.8/site-packages/
-set fish_function_path $fish_function_path "$REPO_ROOT/powerline/bindings/fish"
-powerline-setup
+set -x DOCKER_HOST tcp://localhost:2375
+
+#set PYTHON_VER python3.6
+#set PYTHON_ROOT ~/.local/lib/$PYTHON_VER/site-packages/
+#set fish_function_path $fish_function_path "$PYTHON_ROOT/powerline/bindings/fish"
+#powerline-setup
 
 alias ll="ls -ltrah"
 alias open="xdg-open"
@@ -20,6 +19,8 @@ alias sbcl="rlwrap sbcl"
 alias hy="hy --repl-output-fn=hy.contrib.hy-repr.hy-repr"
 alias emacs="env TERM=xterm-24bits /usr/bin/emacs"
 alias vim="vim -u ~/.vimrc-vim.nox"
+alias python=python3.8
+alias python3=python3.8
 
 source $HOME/.keychain/LV59ZN5Y2-fish
 
