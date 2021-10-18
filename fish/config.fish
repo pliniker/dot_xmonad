bc010feaa44d66c8636b7358922ca2c246324366
Peter Liniker <peter.liniker+github@gmail.com>
2021-10-18 13:06:47 -0400

    Remove vi mode from fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,27 +1,22 @@
-fish_vi_key_bindings
-set fish_cursor_default block
-set fish_cursor_insert line
-set fish_cursor_replace_one underscore
-set fish_cursor_visual block
-
 set -x PATH $HOME/bin $PATH
 set -x PATH $HOME/local/bin $PATH
 set -x PATH $HOME/.cargo/bin $PATH
+set -x PATH $HOME/.npm/modules/bin $PATH
+set -x PATH $HOME/.gems/bin $PATH
 
 set -x NO_AT_BRIDGE 1
+set -x NPM_CONFIG_PREFIX $HOME/.npm/modules/
+set -x GEM_HOME ~/.gems
 
 set -x TERM 'xterm-256color'
 
-alias ll="exa -la --sort changed --icons"
-alias open="xdg-open"
 alias sbcl="rlwrap sbcl"
 alias hy="hy --repl-output-fn=hy.contrib.hy-repr.hy-repr"
+
+alias ll="exa -la --sort changed --icons"
+alias open="xdg-open"
+
 alias emacs="env TERM=xterm-24bits /usr/bin/emacs -nw"
-
-# vanilla vim, don't load spacevim
 alias vim="vim.nox -u ~/.vimrc-vim.nox"
-
-set -x GEM_HOME ~/.gems
-set -x PATH $HOME/.gems/bin $PATH
 
 starship init fish | source
