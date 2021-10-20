f3f37c6d468f53ab6a775611309893b2eaf5a735
Peter Liniker <pliniker@buildfax.com>
2021-10-20 08:59:19 -0400

    Update keymap for nvim; setup X forwarding in WSL2

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,7 @@
+fish_default_key_bindings
+
 set -x PATH $HOME/bin $PATH
-set -x PATH $HOME/local/bin $PATH
+set -x PATH $HOME/.local/bin $PATH
 set -x PATH $HOME/.cargo/bin $PATH
 set -x PATH $HOME/.local/bin $PATH
 set -x PATH $HOME/.npm/modules/bin $PATH
@@ -10,14 +12,18 @@ set -x NPM_CONFIG_PREFIX $HOME/.npm/modules/
 set -x TERM 'xterm-256color'
 # set -x TERM 'xterm-24bit'
 
+set -x DISPLAY=(awk '/nameserver / {print $2; exit}' /etc/resolv.conf 2>/dev/null):0
+set -x LIBGL_ALWAYS_INDIRECT=1
+
 alias sbcl="rlwrap sbcl"
 alias hy="hy --repl-output-fn=hy.contrib.hy-repr.hy-repr"
 
+alias ls=exa
 alias ll="exa -la -s modified --icons"
 alias open="xdg-open"
 alias emacs="env TERM=xterm-24bits /usr/bin/emacs"
 alias vim="nvim -u ~/.vimrc-vim.nox"
 
-source ~/.keychain/LV59ZN5Y2-fish
+source ~/.keychain/*-fish
 
 starship init fish | source
