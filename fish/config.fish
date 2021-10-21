be57a1c4ba043a2f5db89db9b78b5881a21ff72f
Peter Liniker <pliniker@buildfax.com>
2021-10-21 12:32:10 -0400

    Fix bugs in fish config

--- fish/config.fish
+++ fish/config.fish
@@ -9,11 +9,15 @@ set -x PATH $HOME/.npm/modules/bin $PATH
 set -x NO_AT_BRIDGE 1
 set -x NPM_CONFIG_PREFIX $HOME/.npm/modules/
 
-set -x TERM 'xterm-256color'
+# Term xterm-24bit everywhere breaks too many things, only needed for emacs
 # set -x TERM 'xterm-24bit'
+set -x TERM 'xterm-256color'
 
-set -x DISPLAY=(awk '/nameserver / {print $2; exit}' /etc/resolv.conf 2>/dev/null):0
-set -x LIBGL_ALWAYS_INDIRECT=1
+# If WSL2 environment, set DISPLAY
+if test -n "$WSLENV"
+    set -x DISPLAY (awk '/nameserver / {print $2; exit}' /etc/resolv.conf 2>/dev/null):0
+    set -x LIBGL_ALWAYS_INDIRECT 1
+end
 
 alias sbcl="rlwrap sbcl"
 alias hy="hy --repl-output-fn=hy.contrib.hy-repr.hy-repr"
@@ -24,6 +28,8 @@ alias open="xdg-open"
 alias emacs="env TERM=xterm-24bits /usr/bin/emacs"
 alias vim="nvim -u ~/.vimrc-vim.nox"
 
-source ~/.keychain/*-fish
+if test -d ~/.keychain/
+    source ~/.keychain/*-fish
+end
 
 starship init fish | source
